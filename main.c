01ff69d4f0c6c3a012b1c76146823217f2e935d7
Darius Jankauskas <d5926j@gmail.com>
2019-12-03 11:07:46 -0500

    Skeleton main file.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, int **argv) {
+    printf("Welcome to the chocomilk shell! © Darius Jankauskas, Sunan Tajwar 2019\n");
+    printf("For help, type help <command_name>.\n");
+
+    return 0;
+}
