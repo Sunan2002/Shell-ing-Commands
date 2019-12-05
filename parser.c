3773fe456f1ef24d323cf033dca586387ca89ff5
Darius Jankauskas <d5926j@gmail.com>
2019-12-05 10:21:59 -0500

    Simple impl of parser.

--- parser.c
+++ parser.c
@@ -2,10 +2,32 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char** split_input(char *input) {
-    //TODO: implement
-    return NULL;
+    size_t cap_args = 8;
+    size_t num_args = 0;
+    char **args = calloc(cap_args, sizeof(char*));
+
+    char *token = strtok(input, " ");
+
+    while(token  != NULL) {
+
+        //realloc args buffer if too many args
+        if(cap_args - 1 == num_args) {
+            cap_args *= 2;
+            args = realloc(args, cap_args * sizeof(char*));
+        }
+
+        printf("cap_args: %d, num_args: %d, token: %s\n", cap_args, num_args, token);
+
+        args[num_args++] = token;
+
+        token = strtok(NULL, " ");
+    }
+    
+    args[num_args] = NULL;
+    return args;
 }
 
 void strip_newline(char *input) {
