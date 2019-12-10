69101e4c960a3792c042e4f65d77f680e61caeda
Darius Jankauskas <d5926j@gmail.com>
2019-12-09 20:54:49 -0500

    Add redirect support, buggy sigint handling.

--- exec.c
+++ exec.c
@@ -1,5 +1,6 @@
 #include "exec.h"
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -7,39 +8,185 @@
 #include <stdbool.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
 
 _Bool exec_special(char **args);
 
+void exec_single_program_args(char ** args, int stdin, int stdout, int stderr);
+
 void exec_args(char ** args) {
-    pid_t child, parent;
+    int stdout_fd = STDOUT_FILENO;
+    int stdin_fd = STDIN_FILENO;
+    int stderr_fd = STDERR_FILENO;
+
+    char **program_args = args;
+
+    if(!args) return;
+    for(char **pos = args; *pos != NULL; pos++) {
+        if (strcmp(*pos, ">") == 0 || strcmp(*pos, "1>") == 0) {
+            if(pos[1] == NULL) {
+                printf("Error: must specify file to redirect to.\n");
+                return;
+            }
+            int fd = open(pos[1], O_CREAT | O_TRUNC | O_RDWR);
+            if (fd < 0) {
+                printf("Error: %s\n", strerror(errno));
+                return;
+            }
+            stdout_fd = fd;
+            *pos = NULL;
+            break;
+        }
+        
+        else if (strcmp(*pos, "2>") == 0) {
+            if(pos[1] == NULL) {
+                printf("Error: must specify file to redirect to.\n");
+                return;
+            }
+            int fd = open(pos[1], O_CREAT | O_TRUNC | O_RDWR);
+            if (fd < 0) {
+                printf("Error: %s\n", strerror(errno));
+                return;
+            }
+            stderr_fd = fd;
+            *pos = NULL;
+            break;
+        }
+        else if (strcmp(*pos, "&>") == 0) {
+            printf("test: found >>\n");
+            if(pos[1] == NULL) {
+                printf("Error: must specify file to redirect to.\n");
+                return;
+            }
+            int fd = open(pos[1], O_CREAT | O_TRUNC | O_RDWR);
+            if (fd < 0) {
+                printf("Error: %s\n", strerror(errno));
+                return;
+            }
+            stdout_fd = fd;
+            stderr_fd = fd;
+            *pos = NULL;
+            break;
+        }
+
+        else if (strcmp(*pos, ">>") == 0 || strcmp(*pos, "1>>") == 0) {
+            printf("test: found >>\n");
+            if(pos[1] == NULL) {
+                printf("Error: must specify file to redirect to.\n");
+                return;
+            }
+            int fd = open(pos[1], O_CREAT | O_APPEND | O_RDWR);
+            if (fd < 0) {
+                printf("Error: %s\n", strerror(errno));
+                return;
+            }
+            stdout_fd = fd;
+            *pos = NULL;
+            break;
+        }
+        else if (strcmp(*pos, "2>>") == 0) {
+            printf("test: found >>\n");
+            if(pos[1] == NULL) {
+                printf("Error: must specify file to redirect to.\n");
+                return;
+            }
+            int fd = open(pos[1], O_CREAT | O_APPEND | O_RDWR);
+            if (fd < 0) {
+                printf("Error: %s\n", strerror(errno));
+                return;
+            }
+            stderr_fd = fd;
+            *pos = NULL;
+            break;
+        }
+        else if (strcmp(*pos, "&>>") == 0) {
+            printf("test: found >>\n");
+            if(pos[1] == NULL) {
+                printf("Error: must specify file to redirect to.\n");
+                return;
+            }
+            int fd = open(pos[1], O_CREAT | O_APPEND | O_RDWR);
+            if (fd < 0) {
+                printf("Error: %s\n", strerror(errno));
+                return;
+            }
+            stdout_fd = fd;
+            stderr_fd = fd;
+            *pos = NULL;
+            break;
+        }
+
+        else if (strcmp(*pos, ";") == 0) {
+            *pos = NULL;
+            exec_single_program_args(program_args, stdin_fd, stdout_fd, stderr_fd);
+            exec_single_program_args(pos + 1, STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO);
+            return;
+        }
+    }
+    exec_single_program_args(args, stdin_fd, stdout_fd, stderr_fd);
+
+    if (stdin_fd != STDIN_FILENO) {
+        close(stdin_fd);
+    }
+    if (stdout_fd != STDOUT_FILENO) {
+        close(stdout_fd);
+    }
+    if (stderr_fd != STDERR_FILENO) {
+        close(stderr_fd);
+    }
+}
+
+//args: program name and its arguments
+//int stdin, stdout, stderr: file descriptors to use for child
+void exec_single_program_args(char ** args, int stdin, int stdout, int stderr) {
+    pid_t parent;
     int status;
 
     if(exec_special(args)) return;
 
-    child = fork();
-    if (child == 0) {
+    child_process = fork();
+    if (child_process == 0) {
+        dup2(stdin, STDIN_FILENO);
+        dup2(stdout, STDOUT_FILENO);
+        dup2(stderr, STDERR_FILENO);
         if (execvp(args[0], args) < 0)
-            printf("\nError: Execution failed.\n");
+            printf("Error: Execution failed.\n");
+        exit(0);
     }
 
-    else if (child == -1) printf("\nError: Child could not be forked.\n");
-    else parent = waitpid(child, &status, 0);
-    //return 1;
+    else if (child_process == -1) printf("Error: child could not be forked.\n");
+    else {
+        parent = waitpid(child_process, &status, 0);
+        //sleep(1);
+        printf("  test: waitpid returned  ");
+    }
+    
+    //indicate to terminate_exec no process is running
+    child_process = -1;
+}
+
+void terminate_exec() {
+    if(child_process > 0) {
+        kill(child_process, SIGINT);
+    }
 }
 
 //returns true if special command executed
 _Bool exec_special(char **args) {
-    if(strcmp(args[0], "exit") == 0) {
+    if(args[0] == NULL || strcmp(args[0], "") == 0) {
+        return true;
+    }
+    else if(strcmp(args[0], "exit") == 0) {
         //TODO: impl numeric return
         if (args[1] == NULL) exit(0);
         else {
-            printf("Error: exit expects 0 arguments.\n");
+            fprintf(stderr, "Error: exit expects 0 arguments.\n");
             return true;
         }
     }
     else if(strcmp(args[0], "cd") == 0) {
         if(args[1] != NULL && args[2] != NULL) {
-            printf("Error: cd expects 0 or 1 arguments.\n");
+            fprintf(stderr, "Error: cd expects 0 or 1 arguments.\n");
         }
         else if(args[1] == NULL) {
             char *home = getenv("HOME");
@@ -49,12 +196,12 @@ _Bool exec_special(char **args) {
             }
             else chdir("/");
             if(err < 0) {
-                printf("Error: %s.\n", strerror(errno));
+                fprintf(stderr, "Error: %s.\n", strerror(errno));
             }
         }
         else {
             if (chdir(args[1]) < 0) {
-                printf("Error: %s.\n", strerror(errno));
+                fprintf(stderr, "Error: %s.\n", strerror(errno));
             }
         }
         return true;
--- exec.h
+++ exec.h
@@ -1,6 +1,18 @@
 #ifndef __EXEC_INCLUDES
 #define __EXEC_INCLUDES
 
+#include <sys/types.h>
+
+//current child process used to run command
+//-1 if error with child process or no child running
+pid_t child_process;
+
+//takes in array of args, where args[0] is the name of the program,
+//the next args are passed to the program, and args[n_last] == NULL
 void exec_args(char ** args);
+
+//if a child process is executing a program, it is sent SIGTERM
+//otherwise no action taken
+void terminate_exec();
 
 #endif
