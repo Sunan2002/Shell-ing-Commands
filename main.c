3f17448c2ee07916ce8cdc070e233fc6a19f0991
Darius Jankauskas <d5926j@gmail.com>
2019-12-09 20:52:27 -0500

    Remove unneeded contents, add buggy control-c handler.

--- main.c
+++ main.c
@@ -3,22 +3,39 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <signal.h>
 
 #include "parser.h"
 #include "exec.h"
 
-int main(int argc,  **argv) {
+static void sigint_handler(int sig);
+
+int main(int argc, char **argv) {
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+    
     printf("Welcome to the chocomilk shell! © Darius Jankauskas, Sunan Tajwar 2019\n");
     printf("For help, type help <command_name>.\n");
+
+   //VSCODE does not understand this for some reason
+   //does not affect build
+    #ifndef __INTELLISENSE__
+    struct sigaction sig_handler;
+    sig_handler.sa_handler = sigint_handler;
+    sigemptyset(&sig_handler.sa_mask);
+    sig_handler.sa_flags = 0;
+    sigaction(SIGINT, &sig_handler, NULL);
+    #endif
+
 
     //TODO: permit arbitrary size?
     char input[10000];
 
     while(true) {
-        //TODO: print current working directory and user
         char cwd[1000];
         getcwd(cwd, sizeof(cwd));
-        printf("%s", cwd);
+        printf("MS %s> ", cwd);
+        
         //TODO: check for errors
         fgets(input, sizeof input, stdin);
         strip_newline(input);
@@ -35,5 +52,9 @@ int main(int argc,  **argv) {
     }
 
     return 0;
+}
+
+static void sigint_handler(int sig) {
+    terminate_exec();
 }
 
